1fb518d8490aae7fb6e126bdc38fc1a28e3b066b
jeni.skp <jeni.skp@edu.mercantec.dk>
2023-06-16 09:45:52 +0200

    Updated i3 modules

--- configs/i3/modules/1 xrandr.sh
+++ configs/i3/modules/1 xrandr.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-xrandr --output DP-1 --mode 1920x1080 --left-of DVI-I-1 && xrandr --output DP-1-3 --mode 1920x1080 --left-of DP-1
+xrandr --output DP-1-3 --mode 1920x1080 && xrandr --output DVI-I-1 --mode 1920x1080 --primary --left-of DP-1-3 && xrandr --output DP-1-2 --mode 1920x1080 --left-of DVI-I-1
--- configs/i3/modules/2 nvidia.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-nvidia-settings --assign CurrentMetaMode="DPY-1: nvidia-auto-select +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DPY-3: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
