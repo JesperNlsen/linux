8fee674ef93158575edaa5e2bf5d39696ff87b5a
Jesper Nielsen <jeni.skp@edu.mercantec.dk>
2023-08-11 09:51:00 +0200

    updated i3 config

--- configs/i3/modules/1 xrandr.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-xrandr --output DP-1-3 --mode 1920x1080 && xrandr --output DVI-I-1 --mode 1920x1080 --primary --left-of DP-1-3 && xrandr --output DP-1-2 --mode 1920x1080 --left-of DVI-I-1
